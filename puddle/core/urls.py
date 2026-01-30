9ba137fb83b5bbf087742159efd6434d4ee2ce70
Tinsae Befekadu <tinsaebefekadu2012@gmail.com>
2026-01-30 18:58:53 +0300

    updated template with main and contact page

--- puddle/core/urls.py
+++ puddle/core/urls.py
@@ -1,6 +1,9 @@
+from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index,contact
 
 urlpatterns = [
     path('', index, name='index'),
+    path('contact/', contact, name='contact'),
+    path('admin/', admin.site.urls),
 ]
