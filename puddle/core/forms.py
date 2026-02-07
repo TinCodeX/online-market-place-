90b2f01343b0ce01f5b82d30fd574e8dd29501a0
Tinsae Befekadu <tinsaebefekadu2012@gmail.com>
2026-02-07 22:45:55 +0300

    second commit on signup

--- puddle/core/forms.py
+++ puddle/core/forms.py
@@ -1,4 +1,4 @@
-from django import froms 
+from django import forms 
 from django.contrib.auth.forms import UserCreationForm 
 from django.contrib.auth.models import User
 
@@ -6,3 +6,20 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'YourUsername',
+        'class': 'w-full py-4 px-10 rounded-xl'
+    }))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Your email address',
+        'class': 'w-full py-4 px-10 rounded-xl'
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Your password',
+        'class': 'w-full py-4 px-10 rounded-xl'
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm your password',
+        'class': 'w-full py-4 px-10 rounded-xl'
+    }))
